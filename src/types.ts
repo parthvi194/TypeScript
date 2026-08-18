91ddc1ef8a06752fb5a53b03f811596f42532fdf
Parthvi Modi <modiparthvi106@gmail.com>
2026-08-18 06:08:08 +0000

    types in ts

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5
+
+let channelName = "ChaiaurCode"
+
+let chaiFlavour:string = "Masala chai"
+chaiFlavour = "Ginger Tea"
+
+let chaiOrder:number 
